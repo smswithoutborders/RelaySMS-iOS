6d01f0225f9514c5374efde58172398bb8f4b420
sherlock <wisdomnji@gmail.com>
2024-06-13 14:57:24 +0100

    update: may the onboarding begin

---
+++ SMSWithoutBorders-Production/Views/Onboarding/OnboardingWelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  SMSWithoutBorders-Production
+//
+//  Created by sh3rlock on 13/06/2024.
+//
+
+import SwiftUI
+
+struct OnboardingWelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingWelcomeView()
+}
