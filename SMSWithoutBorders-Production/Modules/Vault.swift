0f8a3295e965f73113488baa7bd0ed59101fbb9d
sherlock <wisdomnji@gmail.com>
2024-06-26 22:08:28 +0100

    update: found some error codes for implementation in ios

--- SMSWithoutBorders-Production/Modules/Vault.swift
+++ SMSWithoutBorders-Production/Modules/Vault.swift
@@ -10,27 +10,73 @@ import GRPC
 import Logging
 
 
-class VaultAuth {
+class Vault {
     
     var channel: ClientConnection?
     var callOptions: CallOptions?
+    var vaultEntityStub: Vault_V1_EntityNIOClient?
 
     init() {
         channel = GRPCHandler.getChannel()
         let logger = Logger(label: "gRPC", factory: StreamLogHandler.standardOutput(label:))
         callOptions = CallOptions.init(logger: logger)
+        vaultEntityStub = Vault_V1_EntityNIOClient.init(channel: channel!,
+                                                            defaultCallOptions: callOptions!)
     }
     
-    func createEntity(phoneNumber: String, countryCode: String) throws -> Vault_V1_CreateEntityResponse{
-        let vaultEntityStub = Vault_V1_EntityNIOClient.init(channel: channel!,
-                                                            defaultCallOptions: callOptions!)
-        var message = ""
-
+    func createEntity(phoneNumber: String) throws -> Vault_V1_CreateEntityResponse? {
         let entityCreationRequest: Vault_V1_CreateEntityRequest = .with {
             $0.phoneNumber = phoneNumber
         }
         
-        let call = vaultEntityStub.createEntity(entityCreationRequest)
+        let call = vaultEntityStub!.createEntity(entityCreationRequest)
+        let response: Vault_V1_CreateEntityResponse?
+        
+        do {
+            response = try call.response.wait()
+            let status = try call.status.wait()
+            
+            print("status code - raw value: \(status.code.rawValue)")
+            print("status code - description: \(status.code.description)")
+            print("status code - isOk: \(status.isOk)")
+        } catch {
+            print("Some error came back: \(error)")
+            throw error
+        }
+
+        return response
+    }
+    
+    func createEntity2(phoneNumber: String, 
+                       countryCode: String, 
+                       password: String, 
+                       clientPublishPubKey: String,
+                       clientDeviceIdPubKey: String,
+                       ownershipResponse: String) throws -> Vault_V1_CreateEntityResponse{
+        let entityCreationRequest: Vault_V1_CreateEntityRequest = .with {
+            $0.countryCode = countryCode
+            $0.phoneNumber = phoneNumber
+            $0.password = password
+            $0.clientPublishPubKey = clientPublishPubKey
+            $0.clientDeviceIDPubKey = clientDeviceIdPubKey
+            $0.ownershipProofResponse = ownershipResponse
+        }
+        
+        let call = vaultEntityStub!.createEntity(entityCreationRequest)
         return try call.response.wait()
+    }
+    
+    func authenticateEntity(phoneNumber: String, password: String) throws {
+        
+    }
+    
+    func authenticateEntity2(phoneNumber: String, 
+                             clientPublishPubKey: String,
+                             clientDeviceIDPubKey: String) throws -> String {
+        return ""
+    }
+    
+    func listStoredEntityToken(longLiveToken: String) throws -> Array<Vault_V1_Token> {
+        return []
     }
 }
--- SMSWithoutBorders-ProductionTests/VaultTest.swift
+++ SMSWithoutBorders-ProductionTests/VaultTest.swift
@@ -11,31 +11,65 @@ import NIO
 import XCTest
 import Logging
 
+@testable import SMSWithoutBorders
+
 struct VaultTest {
-
+    var vault = Vault()
+    var phoneNumber = "+2371234567891"
+    var password = "dMd2Kmo9"
+    var ownershipProof = "123456"
+    
+    var clientPublishPubKey = ""
+    var clientDeviceIDPubKey = ""
+    
     @Test func entityCreationTest() throws {
-        let phoneNumber = "+2371234567891"
-        
-        let group = PlatformSupport.makeEventLoopGroup(loopCount: 1, networkPreference: .best)
-        let channel = ClientConnection
-            .usingPlatformAppropriateTLS(for: group)
-            .connect(host:"staging.smswithoutborders.com", port: 9050)
-        
-        let logger = Logger(label: "gRPC", factory: StreamLogHandler.standardOutput(label:))
-        let callOptions = CallOptions.init(logger: logger)
-        
-        let vaultEntityStub = Vault_V1_EntityNIOClient.init(channel: channel, defaultCallOptions: callOptions)
-
-        let entityCreationRequest: Vault_V1_CreateEntityRequest = .with {
-            $0.phoneNumber = phoneNumber
+        do {
+            let entityCreationResponse = try vault.createEntity(phoneNumber: phoneNumber)
+            
+            XCTAssertTrue(entityCreationResponse != nil)
+            
+            print("message says: \(entityCreationResponse?.message)")
+            
+            XCTAssertTrue(((entityCreationResponse?.requiresOwnershipProof) != nil))
+        } catch {
+            throw error
         }
         
-        let call = vaultEntityStub.createEntity(entityCreationRequest)
-        let entityCreationResponse = try call.response.wait()
-        
-        print("message says: \(entityCreationResponse.message)")
-
-        XCTAssertTrue(entityCreationResponse.requiresOwnershipProof)
     }
-
+    
+    @Test func entityCreation2Test() throws {
+        let countryCode = "CM"
+        
+        let entityCreationResponse = try vault.createEntity2(
+            phoneNumber: phoneNumber,
+            countryCode: countryCode,
+            password: password,
+            clientPublishPubKey: clientPublishPubKey,
+            clientDeviceIdPubKey: clientDeviceIDPubKey,
+            ownershipResponse: ownershipProof)
+        print("message says: \(entityCreationResponse.message)")
+        
+        XCTAssertFalse(entityCreationResponse.requiresOwnershipProof)
+    }
+    
+    
+    @Test func authenticateEntity() throws {
+        let authenticationResponse = try vault.authenticateEntity(
+            phoneNumber: phoneNumber, password: password)
+        
+        try vault.authenticateEntity2(phoneNumber: phoneNumber,
+                                      clientPublishPubKey: clientPublishPubKey,
+                                      clientDeviceIDPubKey: clientDeviceIDPubKey)
+    }
+    
+    @Test func listStoredEntityTokens() throws {
+        let authenticationResponse = try vault.authenticateEntity(
+            phoneNumber: phoneNumber, password: password)
+        
+        let llt = try vault.authenticateEntity2(phoneNumber: phoneNumber,
+                                      clientPublishPubKey: clientPublishPubKey,
+                                      clientDeviceIDPubKey: clientDeviceIDPubKey)
+        
+        let storedTokens = try vault.listStoredEntityToken(longLiveToken: llt)
+    }
 }
