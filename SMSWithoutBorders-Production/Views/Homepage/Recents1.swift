70dd2494c3973f4782c497a1df6df9dc5ee8e963
sherlock <wisdomnji@gmail.com>
2025-01-21 22:30:17 +0100

    update: included more details in the recents page, now to build the cards and see what the modals would look like

--- SMSWithoutBorders-Production/Views/Homepage/Recents1.swift
+++ SMSWithoutBorders-Production/Views/Homepage/Recents1.swift
@@ -7,100 +7,130 @@
 
 import SwiftUI
 
+struct CreateAccountSheetView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "person.crop.circle.badge.plus")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 75, height: 75)
+        }
+    }
+}
+
 struct Recents1: View {
     @State private var sheetCreateAccountIsPresented: Bool = false
     
     var body: some View {
         NavigationView {
-            VStack(spacing: 10) {
-                VStack {
-                    Text("Internet required")
-                        .font(.headline)
-                    Text("This features requires you to have an internet connection")
-                        .font(.caption)
-                        .multilineTextAlignment(.center)
-                }
-                HStack(spacing: 50) {
-                    Button(action: {
-                        sheetCreateAccountIsPresented.toggle()
-                    }) {
-                        VStack {
-                            Image(systemName: "person.crop.circle.badge.plus")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 75, height: 75)
-                            Text("Create Account")
-                                .font(.caption)
-                                .foregroundColor(.primary)
+            ScrollView {
+                VStack(spacing: 10) {
+                    VStack {
+                        Text("Internet required")
+                            .font(.headline)
+                        Text("These features requires you to have an internet connection")
+                            .font(.caption)
+                            .multilineTextAlignment(.center)
+                    }
+                    HStack(spacing: 50) {
+                        Button(action: {
+                            sheetCreateAccountIsPresented.toggle()
+                        }) {
+                            VStack {
+                                Image(systemName: "person.crop.circle.badge.plus")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 75, height: 75)
+                                Text("Create Account")
+                                    .font(.caption)
+                                    .foregroundColor(.primary)
+                            }
                         }
-                    }
-                    .buttonStyle(.bordered)
-                    .tint(.accentColor)
-                    .sheet(isPresented: $sheetCreateAccountIsPresented) {
-                        Text("Hello world")
-                    }
+                        .buttonStyle(.bordered)
+                        .tint(.accentColor)
+                        .sheet(isPresented: $sheetCreateAccountIsPresented) {
+                            CreateAccountSheetView()
+                        }
 
-                    Button(action: {}) {
-                        VStack {
-                            Image(systemName: "person.crop.circle.badge")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 75, height: 75)
-                            Text("Log in")
-                                .font(.caption)
-                                .foregroundColor(.primary)
+                        Button(action: {}) {
+                            VStack {
+                                Image(systemName: "person.crop.circle.badge")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 75, height: 75)
+                                Text("Log in")
+                                    .font(.caption)
+                                    .foregroundColor(.primary)
+                            }
                         }
+                        .buttonStyle(.bordered)
+                        .tint(.accentColor)
                     }
-                    .buttonStyle(.bordered)
-                    .tint(.accentColor)
-                }
-                .padding()
-                
-                Divider()
-                    .padding(.bottom, 16)
-                
-                VStack {
-                    Text("Offline features")
-                        .font(.headline)
-                    Text("This features work without an internet connection!")
-                        .font(.caption)
-                        .multilineTextAlignment(.center)
-                }
-                HStack(spacing: 50) {
+                    .padding()
+                    
+                    Divider()
+                        .padding(.bottom, 16)
+                    
+                    VStack {
+                        Text("Offline features")
+                            .font(.headline)
+                        Text("These features work without an internet connection!")
+                            .font(.caption)
+                            .multilineTextAlignment(.center)
+                    }
+                    HStack(spacing: 50) {
+                        Button(action: {}) {
+                            VStack {
+                                Image(systemName: "pencil.circle")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 75, height: 75)
+                                Text("Try example")
+                                    .font(.caption)
+                                    .foregroundColor(.primary)
+                            }
+                        }
+                        .buttonStyle(.bordered)
+                        .tint(.accentColor)
+                        
+                        Button(action: {}) {
+                            VStack {
+                                Image(systemName: "wifi.slash")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 75, height: 75)
+                                Text("SMS Login")
+                                    .font(.caption)
+                                    .foregroundColor(.primary)
+                            }
+                        }
+                        .buttonStyle(.bordered)
+                        .tint(.accentColor)
+                    }
+                    .padding()
+                    
+                    Divider()
+                    
                     Button(action: {}) {
                         VStack {
                             Image("Logo")
                                 .resizable()
                                 .scaledToFit()
                                 .frame(width: 75, height: 75)
-                            Text("Try RelaySMS")
+                            Text("Tutorial")
                                 .font(.caption)
                                 .foregroundColor(.primary)
                         }
                     }
                     .buttonStyle(.bordered)
                     .tint(.accentColor)
-                    
-                    Button(action: {}) {
-                        VStack {
-                            Image(systemName: "wifi.slash")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 75, height: 75)
-                            Text("SMS Login")
-                                .font(.caption)
-                                .foregroundColor(.primary)
-                        }
-                    }
-                    .buttonStyle(.bordered)
-                    .tint(.accentColor)
+                    .padding(.top)
                 }
+                .navigationTitle("Not logged in")
                 .padding()
+                
             }
-            .navigationTitle("RelaySMS")
-            .padding()
         }
-        
     }
 }
 
