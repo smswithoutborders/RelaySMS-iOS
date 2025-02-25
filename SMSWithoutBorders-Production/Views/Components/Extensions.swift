686df586924e1ce74d39816f3947a14e1291289c
sherlock <wisdomnji@gmail.com>
2025-02-25 21:12:15 +0100

    update: removed some files, now to fix Telegram

--- SMSWithoutBorders-Production/Views/Components/Extensions.swift
+++ SMSWithoutBorders-Production/Views/Components/Extensions.swift
@@ -17,3 +17,17 @@ extension View {
         }
     }
 }
+
+extension DispatchQueue {
+    static func background(delay: Double = 0.0, background: (()->Void)? = nil, completion: (() -> Void)? = nil) {
+        DispatchQueue.global(qos: .background).async {
+            background?()
+            if let completion = completion {
+                DispatchQueue.main.asyncAfter(deadline: .now() + delay, execute: {
+                    completion()
+                })
+            }
+        }
+    }
+
+}
