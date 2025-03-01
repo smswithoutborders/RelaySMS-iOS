cc7d08953a1d8fa26cb0e66f9750ff2161318137
sherlock <wisdomnji@gmail.com>
2025-03-01 18:47:43 +0100

    update: in the final form of languaging

--- SMSWithoutBorders-Production/Views/Onboarding/OnboardingWelcomeView.swift
+++ SMSWithoutBorders-Production/Views/Onboarding/OnboardingWelcomeView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct OnboardingWelcomeView: View {
+    @Environment(\.locale) var locale
     @Binding var pageIndex: Int
+    
+    @State private var showLanguageChangeConfirmationAlert = false
     
     var body: some View {
         VStack {
             Text("Welcome to RelaySMS!")
                 .font(Font.custom("unbounded", size: 18))
                 .fontWeight(.semibold)
-                .padding(.top, 40)
+                .padding(.top, 30)
                 
                 
             VStack {
@@ -24,13 +27,51 @@ struct OnboardingWelcomeView: View {
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 300, height: 300)
                     .padding()
-                
-                LanguageSelectorButtonView()
+                    .padding(.bottom, 20)
+
+
+                Button {
+                    showLanguageChangeConfirmationAlert = true
+                } label: {
+                    if #available(iOS 16, *) {
+                        if let languageCode = locale.language.languageCode?.identifier,
+                           let languageName = Locale.current.localizedString(forLanguageCode: languageCode) {
+                            Label(languageName, systemImage: "globe")
+                                .padding(12)
+                                .background(Color.blue.opacity(0.1))
+                                .clipShape(Capsule())
+                        }
+                            
+                    } else {
+                        if let languageCode = locale.languageCode,
+                           let languageName = Locale.current.localizedString(forLanguageCode: languageCode) {
+                            Label(languageName, systemImage: "globe")
+                                .padding(12)
+                                .background(Color.blue.opacity(0.1))
+                                .clipShape(Capsule())
+                        }
+                    }
+                }
+                .alert("Change App Language", isPresented: $showLanguageChangeConfirmationAlert) {
+                    Button("Cancel", role: .cancel){
+                        showLanguageChangeConfirmationAlert = false
+                    }
+                    Button("Open Settings"){
+                        // Open language settings page instead
+                        if let url: URL = URL(string: UIApplication.openSettingsURLString), UIApplication.shared.canOpenURL(url) {
+                            UIApplication.shared.open(url)
+                        }
+                        
+                    }
+                } message: {
+                    Text(String(localized: "Continue to iOS settings and select your preferred language for RelaySMS.", comment: "Instructions for chnaging application langueg via system settings.") )
+                }
+                .padding(.bottom, 10)
 
 
                 Text(String(localized: "Use SMS to make a post, send emails and messages with no internet connection", comment: "Explains that you can use Relay to make posts, and send emails and messages without an internet conenction"))
                     .font(Font.custom("unbounded", size: 18))
-                    .padding(.bottom, 30)
+                    .padding(.bottom, 10)
                     .multilineTextAlignment(.center)
                     .foregroundColor(.secondary)
                 
@@ -61,50 +102,3 @@ struct OnboardingWelcomeView: View {
     OnboardingWelcomeView(pageIndex: $pageIndex)
 }
 
-
-
-struct LanguageSelectorButtonView: View {
-    @EnvironmentObject var languageManager: LanguagePreferencesManager
-    @State private var selectedLang: Language
-    @State private var showLanguageChangeConfirmationAlert = false
-    
-    init() {
-        // Gets current language code from user preferences
-        let currentCode: String = LanguagePreferencesManager.getStoredLanguageCode()
-        // Sets selectedLanguage code as current language or defaults to English
-       selectedLang = LanguagePreferencesManager.getLanguageFromCode(langCode: currentCode) ?? Language(label: "English", endonym: "English", code: "en") // Should refactor this to use some sort of enhanced enums
-        _selectedLang = State(initialValue: selectedLang)
-    }
-    
-
-    var body: some View {
-        Button(
-            action: {
-                showLanguageChangeConfirmationAlert = true
-            },
-            label: {
-                Label(LanguagePreferencesManager.getLanguageFromCode(langCode: String(languageManager.currentLanguageCode.split(separator: "-").first ?? "n/a"))?.endonym ?? "Select Language", systemImage: "globe")
-                    .padding(12)
-                    .background(Color.blue.opacity(0.1))
-                    .clipShape(Capsule())
-            })
-        .alert("Change App Language", isPresented: $showLanguageChangeConfirmationAlert) {
-            Button("Cancel", role: .cancel){
-                showLanguageChangeConfirmationAlert = false
-            }
-            Button("Open Settings"){
-                // Open language settings page instead
-                if let url: URL = URL(string: UIApplication.openSettingsURLString), UIApplication.shared.canOpenURL(url) {
-                    UIApplication.shared.open(url)
-                }
-                
-            }
-        } message: {
-            Text(String(localized: "Continue to iOS settings and select your preferred language for RelaySMS.", comment: "Instructions for chnaging application langueg via system settings.") )
-        }
-    }
-}
-
-#Preview {
-    LanguageSelectorButtonView()
-}
